8deb329dcff3256a5837074cd565f3859cc75a5f
Adrineel-Saha <adrineelsaha@gmail.com>
2026-01-09 14:52:33 +0530

    Added UserRepository Implementation

---
+++ src/main/java/com/cognizant/userservice/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cognizant.userservice.repositories;
+
+import com.cognizant.userservice.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
