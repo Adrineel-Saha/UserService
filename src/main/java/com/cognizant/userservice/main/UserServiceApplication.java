ce4283260775e75e84f9aeb0e6fee904380dbcaa
Adrineel-Saha <adrineelsaha@gmail.com>
2026-01-28 18:00:32 +0530

    User Service Spring Cloud Configuration Changes

--- src/main/java/com/cognizant/userservice/main/UserServiceApplication.java
+++ src/main/java/com/cognizant/userservice/main/UserServiceApplication.java
@@ -6,12 +6,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.cognizant.userservice.*")
 @EnableJpaRepositories(basePackages = "com.cognizant.userservice.repositories")
 @EntityScan(basePackages = "com.cognizant.userservice.entities")
+@EnableDiscoveryClient(autoRegister=true)
 @OpenAPIDefinition(
 		info=@Info(
 				title="User Service REST API Documentation",
--- src/test/java/com/cognizant/userservice/test/main/UserServiceApplicationTests.java
+++ src/test/java/com/cognizant/userservice/test/main/UserServiceApplicationTests.java
@@ -1,14 +1,21 @@
 package com.cognizant.userservice.test.main;
 
+import com.cognizant.userservice.controllers.UserServiceController;
 import com.cognizant.userservice.main.UserServiceApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes= UserServiceApplication.class)
 class UserServiceApplicationTests {
+	@Autowired
+	private UserServiceController userServiceController;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(userServiceController);
 	}
 
 }
