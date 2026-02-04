261e904093f58f746f615a42b5936672b5d6afff
Adrineel-Saha <adrineelsaha@gmail.com>
2026-02-04 14:52:45 +0530

    Repository Changes

--- src/test/java/com/cognizant/userservice/test/repositories/TestUserRepository.java
+++ src/test/java/com/cognizant/userservice/test/repositories/TestUserRepository.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
