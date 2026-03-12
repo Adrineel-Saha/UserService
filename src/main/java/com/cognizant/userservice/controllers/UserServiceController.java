1296495957bb742d8fb844e7b1c1b1e1120f090b
Adrineel-Saha <adrineelsaha@gmail.com>
2026-03-12 15:50:39 +0530

    Added cross origins for UserServiceController

--- src/main/java/com/cognizant/userservice/controllers/UserServiceController.java
+++ src/main/java/com/cognizant/userservice/controllers/UserServiceController.java
@@ -22,7 +22,7 @@ import java.util.List;
         name="CRUD REST APIs for User Service",
         description="CRUD REST APIs - Create User, Get User, Update User, Delete User"
 )
-
+@CrossOrigin(originPatterns = { "http://*:9191", "https://*:9191" })
 public class UserServiceController {
     @Autowired
     private UserService userService;
