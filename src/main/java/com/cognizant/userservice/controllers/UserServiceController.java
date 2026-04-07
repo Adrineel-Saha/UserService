5c922c45f097c9eae545cbb8f08352e785f4802a
Adrineel-Saha <adrineelsaha@gmail.com>
2026-04-07 09:58:24 +0530

    Added Port number for Angular

--- src/main/java/com/cognizant/userservice/controllers/UserServiceController.java
+++ src/main/java/com/cognizant/userservice/controllers/UserServiceController.java
@@ -22,7 +22,7 @@ import java.util.List;
         name="CRUD REST APIs for User Service",
         description="CRUD REST APIs - Create User, Get User, Update User, Delete User"
 )
-@CrossOrigin(originPatterns = { "http://*:9191", "https://*:9191" })
+@CrossOrigin(originPatterns = { "http://*:9191", "https://*:9191", "http://*:4200", "https://*:4200" })
 public class UserServiceController {
     @Autowired
     private UserService userService;
