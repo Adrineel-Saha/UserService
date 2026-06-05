2db407bef64f390c649debce0e3709be3784038b
Adrineel-Saha <adrineelsaha@gmail.com>
2026-06-05 13:29:04 +0530

    UserService Auth Header Swagger

--- src/main/java/com/cognizant/userservice/main/UserServiceApplication.java
+++ src/main/java/com/cognizant/userservice/main/UserServiceApplication.java
@@ -2,6 +2,11 @@ package com.cognizant.userservice.main;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +14,8 @@ import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.List;
 
 @SpringBootApplication(scanBasePackages = "com.cognizant.userservice.*")
 @EnableJpaRepositories(basePackages = "com.cognizant.userservice.repositories")
@@ -30,5 +37,23 @@ public class UserServiceApplication {
 	public ModelMapper modelMapper()
 	{
 		return new ModelMapper();
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.servers(List.of(
+						new Server().url("http://localhost:9191").description("API Gateway")
+				))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+				.components(new Components()
+						.addSecuritySchemes("bearerAuth",
+								new SecurityScheme()
+										.name("bearerAuth")
+										.type(SecurityScheme.Type.HTTP)
+										.scheme("bearer")
+										.bearerFormat("JWT")
+						)
+				);
 	}
 }
