58bcc22db14559872583a77cb9b0c6a4c61dc1e2
Adrineel-Saha <adrineelsaha@gmail.com>
2026-06-10 18:10:41 +0530

    Added test cases

--- src/test/java/com/cognizant/userservice/test/globalexceptionhandler/TestGlobalExceptionHandler.java
+++ src/test/java/com/cognizant/userservice/test/globalexceptionhandler/TestGlobalExceptionHandler.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class TestGlobalExceptionHandler {
+public class TestGlobalExceptionHandler {
 
     @Mock
     private ResourceNotFoundException resourceNotFoundException;
